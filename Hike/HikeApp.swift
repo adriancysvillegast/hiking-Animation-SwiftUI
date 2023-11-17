b5c3c97ee7854d885587ac599d85cbb3a0cfc0e7
Adriancys Villegas <adriancysvillegast@gmail.com>
2023-11-17 14:00:34 -0400

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Adriancys Jesus Villegas Toro on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
